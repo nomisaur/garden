f010c7145195d314fe53964f2d3f47ffe82d595c
Naomi Embers <naomisaltis@gmail.com>
2020-11-29 16:38:34 -0800

    move bundle

--- webpack.config.js
+++ webpack.config.js
@@ -29,22 +29,16 @@ module.exports = (env, argv) => {
     },
     output: {
       path: resolvePath('./dist'),
-      filename: 'bundle.js',
+      filename: 'build/bundle.js',
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
       new webpack.DefinePlugin({ webpackConfig: { isDev } }),
       new CircularDependencyPlugin({
-        // exclude detection of files based on a RegExp
         exclude: /a\.js|node_modules/,
-        // include specific files based on a RegExp
         include: /src/,
-        // add errors to webpack instead of warnings
         failOnError: true,
-        // allow import cycles that include an asyncronous import,
-        // e.g. via import(/* webpackMode: "weak" */ './file.js')
         allowAsyncCycles: false,
-        // set the current working directory for displaying module paths
         cwd: process.cwd(),
       }),
     ],
