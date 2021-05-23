3a41c64148d247e5c079e54a084c33ac839296ed
embers <naomisaltis@gmail.com>
2021-05-23 00:15:45 -0700

    lists added

--- src/playground.js
+++ src/playground.js
@@ -1,21 +1,68 @@
 const getType = (state, filterType, getVal = (a) => a) => {
-  //const isList = Array.isArray(state);
   return Object.entries(state)
     .filter(([, { type }]) => type === filterType)
     .reduce((grow, [prop, leaf]) => ({ ...grow, [prop]: getVal(leaf) }), {});
 };
 
 const node = (state) => {
-  const mutable = getType(state, 'mutable', (a) => a.val);
+  //const mutable = getType(state, 'mutable', (a) => a.val);
+  //const immutable = getType(state, 'immutable', (a) => a.val);
+  const mutable = Object.entries(state).reduce((grow, [prop, leaf]) => {
+    if (leaf.type === 'mutable') {
+      grow[prop] = leaf.val;
+    }
+    return grow;
+  }, {});
 
-  const immutable = getType(state, 'immutable', (a) => a.val);
+  const immutable = Object.entries(state).reduce((grow, [prop, leaf]) => {
+    if (leaf.type === 'immutable') {
+      grow[prop] = leaf.val;
+    }
+    return grow;
+  }, {});
 
-  const getState = () => ({
+  /* const getState = () => ({
     ...mutable,
     ...getType(state, 'node', (a) => a.getState()),
-  });
+  }); */
+
+  const getState = () => {
+    return Object.entries(state).reduce((grow, [prop, leaf]) => {
+      if (leaf.type === 'mutable') {
+        grow[prop] = mutable[prop];
+      }
+      if (leaf.type === 'node' || leaf.type === 'list') {
+        grow[prop] = leaf.getState();
+      }
+      return grow;
+    }, {});
+  };
 
   const getProps = () => {
+    const realValues = Object.entries(state).reduce((grow, [prop, leaf]) => {
+      if (leaf.type === 'mutable') {
+        grow[prop] = mutable[prop];
+      }
+      if (leaf.type === 'immutable') {
+        grow[prop] = immutable[prop];
+      }
+      if (leaf.type === 'node' || leaf.type === 'list') {
+        grow[prop] = leaf.getProps();
+      }
+      return grow;
+    }, {});
+
+    return Object.entries(state).reduce((grow, [prop, leaf]) => {
+      if (leaf.type === 'virtual') {
+        grow[prop] = leaf.val(realValues);
+      } else {
+        grow[prop] = realValues[prop];
+      }
+      return grow;
+    }, {});
+  };
+
+  /* const getProps = () => {
     const nodeValues = getType(state, 'node', (a) => a.getProps());
     const realValues = {
       ...mutable,
@@ -28,7 +75,7 @@ const node = (state) => {
       ...immutable,
       ...getType(state, 'virtual', (a) => a.val(realValues)),
     };
-  };
+  }; */
 
   const mutate = (fn) => {
     const newState = fn(getState());
@@ -50,10 +97,82 @@ const node = (state) => {
   };
 };
 
+const list = (state) => {
+  const mutable = state.reduce((grow, leaf, index) => {
+    if (leaf.type === 'mutable') {
+      grow[index] = leaf.val;
+    }
+    return grow;
+  }, []);
+
+  const immutable = state.reduce((grow, leaf, index) => {
+    if (leaf.type === 'immutable') {
+      grow[index] = leaf.val;
+    }
+    return grow;
+  }, []);
+
+  const getState = () => {
+    return state.reduce((grow, leaf, index) => {
+      if (leaf.type === 'mutable') {
+        grow[index] = mutable[index];
+      }
+      if (leaf.type === 'node' || leaf.type === 'list') {
+        grow[index] = leaf.getState();
+      }
+      return grow;
+    }, []);
+  };
+
+  const getProps = () => {
+    const realValues = state.reduce((grow, leaf, index) => {
+      if (leaf.type === 'mutable') {
+        grow[index] = mutable[index];
+      }
+      if (leaf.type === 'immutable') {
+        grow[index] = immutable[index];
+      }
+      if (leaf.type === 'node' || leaf.type === 'list') {
+        grow[index] = leaf.getProps();
+      }
+      return grow;
+    }, []);
+
+    return state.reduce((grow, leaf, index) => {
+      if (leaf.type === 'virtual') {
+        grow[index] = leaf.val(realValues);
+      } else {
+        grow[index] = realValues[index];
+      }
+      return grow;
+    }, []);
+  };
+
+  const mutate = (fn) => {
+    const newState = fn(getState());
+    newState.forEach((item, i) => {
+      if (mutable.hasOwnProperty(i)) {
+        mutable[i] = item;
+      } else {
+        state[i].mutate(() => item);
+      }
+    });
+  };
+
+  return {
+    type: 'list',
+    getState,
+    getProps,
+    mutate,
+    state,
+  };
+};
+
 const mut = (val) => ({ val, type: 'mutable' });
 const imm = (val) => ({ val, type: 'immutable' });
 const virt = (val) => ({ val, type: 'virtual' });
-const list = (val) => ({ val, type: 'list' });
+
+//export const state = node({ a: mut(1), b: imm(2) });
 
 /* export const state = node({
   stateA: mut('a'),
@@ -97,16 +216,20 @@ const list = (val) => ({ val, type: 'list' });
   virtA: virt(({ stateA, thingA: { modelA } }) => stateA + modelA),
 }); */
 
-export const state = node([mut(1), imm(2), node({ a: mut(3), b: imm(4) })]);
+export const state = list([
+  imm('immA'),
+  mut('mutA'),
+  virt((beep) => beep[1] + '!!!'),
+  node({ a: mut('mutB'), b: imm('immB') }),
+]);
 
 console.log('getState', state.getState());
 console.log('getProps', state.getProps());
 state.mutate((state) => {
   /* state.stateA = 'z';
-  state.thingA.stateA = 'y'; */
-  //state.listA[0].a = 2;
-
-  console.log(state);
+  state.thingA.stateA = 'y';
+  //state.listA[0].a = 2; */
+  state[1] = 'bob';
   return state;
 });
 console.log('---');
