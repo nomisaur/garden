8e4407fece502080cc31dc9e33d42920ef53762f
embers <naomisaltis@gmail.com>
2022-01-03 20:13:44 -0800

    refactor

---
+++ src/components/music/noteBox/noteBox.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Fraction } from '../../styled/fractions';
+
+import { displayNumber } from '../../../utils';
+
+import { PlayNote } from '../playNote';
+
+const Box = styled.div.attrs(({ color }) => ({
+   style: {
+      background: color,
+   },
+}))`
+   width: 80px;
+   height: 80px;
+   font-size: 12px;
+   margin: 2px;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+`;
+
+const Top = styled.div`
+   display: flex;
+   justify-content: center;
+   font-size: 18px;
+`;
+const Bottom = styled.div`
+   margin-right: 4px;
+   display: flex;
+   justify-content: flex-end;
+`;
+
+export const NoteBox = ({
+   playing,
+   root,
+   ratio: [top, bottom] = [],
+   color,
+   toggleMode = false,
+   longRelease = true,
+   ...props
+}) => {
+   const frequency = (root * top) / bottom;
+
+   return (
+      <Box color={color} {...props}>
+         <div>&nbsp;</div>
+         <Top>
+            <Fraction top={top} bottom={bottom} />
+         </Top>
+         <Bottom>{displayNumber(frequency)}</Bottom>
+
+         <PlayNote
+            playing={playing}
+            frequency={frequency}
+            envelope={{ release: longRelease ? 15 : 2 }}
+            {...props}
+         />
+      </Box>
+   );
+};
--- src/components/music/notes/notes.js
+++ src/components/music/notes/notes.js
@@ -1,70 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-import { Fraction } from '../../styled/fractions';
-
-import { list, displayNumber, reduceFraction } from '../../../utils';
+import { list, reduceFraction } from '../../../utils';
 import { useMusicContext } from '../../../hooks';
 
-import { PlayNote } from '../playNote';
+import { NoteBox } from '../noteBox/noteBox';
 
 const Row = styled.div`
    display: flex;
 `;
-
-const Box = styled.div.attrs(({ color }) => ({
-   style: {
-      background: color,
-   },
-}))`
-   width: 80px;
-   height: 80px;
-   font-size: 12px;
-   margin: 2px;
-   display: flex;
-   flex-direction: column;
-   justify-content: space-between;
-`;
-
-const Top = styled.div`
-   display: flex;
-   justify-content: center;
-   font-size: 18px;
-`;
-const Bottom = styled.div`
-   margin-right: 4px;
-   display: flex;
-   justify-content: flex-end;
-`;
-
-const NoteBox = ({
-   playing,
-   root,
-   ratio: [top, bottom] = [],
-   color,
-   toggleMode = false,
-   longRelease = true,
-   ...props
-}) => {
-   const frequency = (root * top) / bottom;
-
-   return (
-      <Box color={color} {...props}>
-         <div>&nbsp;</div>
-         <Top>
-            <Fraction top={top} bottom={bottom} />
-         </Top>
-         <Bottom>{displayNumber(frequency)}</Bottom>
-
-         <PlayNote
-            playing={playing}
-            frequency={frequency}
-            envelope={{ release: longRelease ? 15 : 2 }}
-            {...props}
-         />
-      </Box>
-   );
-};
 
 const GridNote = ({ root, ratio, gridSize, toggleMode, longRelease }) => {
    const [playing, setPlaying] = useState(false);
